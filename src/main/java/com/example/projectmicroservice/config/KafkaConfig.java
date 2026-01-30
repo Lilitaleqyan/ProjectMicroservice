160be2437347ec133ab2935fd40fd3e1da670a79
user <lilitaleqyan01@mail.ru>
2026-01-30 23:44:20 +0400

    runing new project

---
+++ src/main/java/com/example/projectmicroservice/config/KafkaConfig.java
@@ -0,0 +1,21 @@
+package com.example.projectmicroservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    NewTopic createTopic() {
+       return TopicBuilder.name("payment-created-topic")
+               .partitions(3)
+               .replicas(1)
+               .configs(Map.of("min.insync.replicas", "1"))
+               .build();
+    }
+}
